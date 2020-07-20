959e5ef3763590ac76cb823829f5d017c8eacc06
TimurSagaidakov <66359248+TimurSagaidakov@users.noreply.github.com>
2020-07-20 16:33:18 +0500

    Верстка адаптива

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ $(document).ready(function( ) {
     $('.menu-btn').on('click', function(){
         $(this).toggleClass('close');
         $('.navigation-menu').toggleClass('open');
-        $('body').css('overflow', 'hidden');
+        $('body').toggleClass('lock');
     });
 });
