b82665b378314a6f3582f47db3ddb054f670bd02
TimurSagaidakov <66359248+TimurSagaidakov@users.noreply.github.com>
2020-07-21 19:51:03 +0500

    Сверстал еще блоки, добавил анимацию и адаптив

--- js/main.js
+++ js/main.js
@@ -4,7 +4,30 @@ $(document).ready(function(){
         $('.navigation-menu').toggleClass('open');
         $('body').toggleClass('lock');
     });
-    
+    $('.formulation-item__plus-1').on('click', function(){
+        $('.formulation-item__description-1').toggleClass('active');
+        $('.formulation-item__description-2').removeClass('active');
+        $('.formulation-item__description-3').removeClass('active');
+        $('.formulation-item__description-4').removeClass('active');
+    });
+    $('.formulation-item__plus-2').on('click', function(){
+        $('.formulation-item__description-2').toggleClass('active');
+        $('.formulation-item__description-1').removeClass('active');
+        $('.formulation-item__description-3').removeClass('active');
+        $('.formulation-item__description-4').removeClass('active');
+    });
+    $('.formulation-item__plus-3').on('click', function(){
+        $('.formulation-item__description-3').toggleClass('active');
+        $('.formulation-item__description-1').removeClass('active');
+        $('.formulation-item__description-2').removeClass('active');
+        $('.formulation-item__description-4').removeClass('active');
+    });
+    $('.formulation-item__plus-4').on('click', function(){
+        $('.formulation-item__description-4').toggleClass('active');
+        $('.formulation-item__description-1').removeClass('active');
+        $('.formulation-item__description-2').removeClass('active');
+        $('.formulation-item__description-3').removeClass('active');
+    });
 });
 
    
